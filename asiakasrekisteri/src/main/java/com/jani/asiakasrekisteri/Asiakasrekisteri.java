63163ba632dfc75456b0e975f897b4442193011b
Janicxz <janizu99@gmail.com>
2024-11-19 11:36:50 +0200

    Initial commit

---
+++ asiakasrekisteri/src/main/java/com/jani/asiakasrekisteri/Asiakasrekisteri.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jani.asiakasrekisteri;
+
+/**
+ *
+ * @author Jani
+ */
+public class Asiakasrekisteri {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
